9f760cf7c28da60d81791a64c1d7caeeedcdffb5
sabas <sabas88@gmail.com>
2014-05-22 14:11:42 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,16 +2,16 @@
 // This is a template for a PHP scraper on Morph (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+ require 'scraperwiki.php';
+ require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+ $html = scraperwiki::scrape("http://www1.comune.genova.it/poliziamunicipale/velox/velox2.asp");
 //
 // // Find something on the page using css selectors
 // $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
+$dom->load($html);
+print_r($dom->find("table"));
 //
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
@@ -23,4 +23,17 @@
 // on Morph for PHP (See https://github.com/openaustralia/morph-docker-php) and all that matters
 // is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 // has at least a table called data.
+     
+/*
+foreach($dom->find("div[@align='left'] tr") as $data){
+    $tds = $data->find("td");
+    if(count($tds)==12){
+        $record = array(
+            'country' => $tds[0]->plaintext,
+            'years_in_school' => intval($tds[4]->plaintext)
+        );
+        print json_encode($record) . "\n";
+    }
+}
+*/
 ?>
